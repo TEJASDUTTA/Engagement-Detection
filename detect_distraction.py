14b2573c38f144fdc899b07373293bbb9b8a9ae5
Aviral Gupta <guptaavi352@gmail.com>
2022-03-16 13:17:45 +0530

    Did class implemenation for the code

--- detect_distraction.py
+++ detect_distraction.py
@@ -10,41 +10,36 @@ import imutils
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh()
-distract_model = load_model("model_25img_t1_34v_colab.hdf5", compile=False)
+distract_model = load_model("models/model_25img_t1_34v_colab.hdf5", compile=False)
 img_width, img_height = 40, 40
-
-
-def predict(image, landmarks):
-    roi = image[landmarks[0][1]:landmarks[2][1], landmarks[0][0]:landmarks[2][0]]
-    if roi.any():
-        roi = cv2.resize(roi, (img_width, img_height))
         
-        roi = roi.astype("float") / 255.0
-        roi = img_to_array(roi)
-        roi = np.expand_dims(roi, axis=0)
+class Engagement_Detection:
+    def __init__(self):
+        pass
 
-        prediction = distract_model.predict(roi)
-        return prediction
+    # Distraction Detector
+    def predict(self, image, landmarks):
+        roi = image[landmarks[0][1]:landmarks[2][1], landmarks[0][0]:landmarks[2][0]]
+        if roi.any():
+            roi = cv2.resize(roi, (img_width, img_height))
+            
+            roi = roi.astype("float") / 255.0
+            roi = img_to_array(roi)
+            roi = np.expand_dims(roi, axis=0)
 
-def detect_distraction():
-    cap = cv2.VideoCapture(0)
-    # cap = cv2.VideoCapture("/home/bsar/Downloads/distracted_Trim.mp4.mp4")
-    l=[]
-    while True:
-        ret, image = cap.read()
-        # image = imutils.resize(image, width=400)
-        if ret == False:
-            break
+            prediction = distract_model.predict(roi)
+            return prediction
 
+    def detect_distraction(self, image):
         height, width, _ = image.shape
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
         result = face_mesh.process(rgb_image)
 
         left_eye_landmarks = []
         right_eye_landmarks = []
 
         probs = []
+        probs_mean = 0
         if result.multi_face_landmarks:
             for facial_landmarks in result.multi_face_landmarks:
                 j = 0
@@ -60,34 +55,66 @@ def detect_distraction():
                     j += 1
                     cv2.circle(image, (x,y), 5, (0, 255, 0), -1)
 
+
             draw_lines = [left_eye_landmarks, right_eye_landmarks]
             for i in draw_lines:
                 for j in range(4):
                     cv2.line(image, i[j], i[(j+1)%4], (0, 255, 0), 1)
             
-            probs.append(predict(image, left_eye_landmarks))
-            probs.append(predict(image, right_eye_landmarks))
+            try:
+                probs.append(self.predict(image, left_eye_landmarks))
+                probs.append(self.predict(image, right_eye_landmarks))
 
-            print(probs)
-            probs_mean = np.mean(probs)
+                # print(probs)
+                probs_mean = np.mean(probs)
+
+            except:
+                pass
 
             if probs_mean <= 0.5:
                 cv2.putText(image, "DISTRACTED", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
             1, (0,0,255), 3, cv2.LINE_AA)
-            else:
-                cv2.putText(image, "FOCUSED",(50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-            1, (0,0,255), 3, cv2.LINE_AA)
-            
-        l.append(probs_mean)      
-        cv2.imshow("Image", image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
-    l=np.array(l)
-    print(np.mean(l))
-    cap.release()
-    cv2.destroyAllWindows()
+            else:
+                cv2.putText(image, "FOCUSED", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+            1, (0,0,255), 3, cv2.LINE_AA)
+
+        return probs_mean
+        
+
+    def here_it_goes(self):
+        report = []
+        cap = cv2.VideoCapture(0)
+        while(True):
+            ret, frame = cap.read()
+            if(ret==False):
+                break
+
+            focused_level = self.detect_distraction(frame)
+            # emotion = self.facial_emotion(frame)
+
+            # index = self.get_concentration_index(focused_level, emotion)
+            # report.append(index)
+
+            cv2.imshow("Engagement Detection", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            
+
+        # report = np.array(report)
+        # overall_index = np.mean(report)
+
+        # if(overall_index > 0.65):
+        #     print("You were highly engaged")
+        # elif(overall_index > 0.25):
+        #     print("You were nominally engaged")
+        # else:
+        #     print("Pay attention")
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    detect_distraction()
+    obj = Engagement_Detection()
+    obj.here_it_goes()
