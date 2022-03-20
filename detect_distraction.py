c806990f6a27ba11a6091b71ac25ae1f11591e3f
Aviral Gupta <guptaavi352@gmail.com>
2022-03-20 13:48:08 +0530

    Merged distraction detector with emotion.py

--- detect_distraction.py
+++ detect_distraction.py
@@ -7,12 +7,38 @@ import sys
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 import imutils
-# import dlib
+import dlib
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh()
 distract_model = load_model("models/model_25img_t1_34v_colab.hdf5", compile=False)
 img_width, img_height = 40, 40
+
+emotion_offsets = (20, 40)
+emotions = {
+    0: { "emotion": "Angry",
+        "color": (193, 69, 42) },
+    1: { "emotion": "Disgust",
+        "color": (164, 175, 49) },
+    2: { "emotion": "Fear",
+        "color": (40, 52, 155) },
+    3: { "emotion": "Happy",
+        "color": (23, 164, 28) },
+    4: { "emotion": "Sad",
+        "color": (164, 93, 23) },
+    5: { "emotion": "Suprise",
+        "color": (218, 229, 97) },
+    6: { "emotion": "Neutral",
+        "color": (108, 72, 200) }
+}
+
+faceLandmarks = "shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(faceLandmarks)
+
+emotionModelPath = "models/emotionModel.hdf5"
+emotionClassifier = load_model(emotionModelPath, compile=False)
+emotionTargetSize = emotionClassifier.input_shape[1:3]
         
 class Engagement_Detection:
     def __init__(self):
@@ -83,6 +109,65 @@ class Engagement_Detection:
         return probs_mean
         
 
+    def shapePoints(self, shape):
+        coords = np.zeros((68, 2), dtype="int")
+        for i in range(0, 68):
+            coords[i] = (shape.part(i).x, shape.part(i).y)
+        return coords
+
+
+    def rectPoints(self, rect):
+        x = rect.left()
+        y = rect.top()
+        w = rect.right() - x
+        h = rect.bottom() - y
+        return (x, y, w, h)
+
+    def facial_emotion(self, frame):
+        grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rects = detector(grayFrame, 0)
+        for rect in rects:
+            shape = predictor(grayFrame, rect)
+            points = self.shapePoints(shape)
+            (x, y, w, h) = self.rectPoints(rect)
+            grayFace = grayFrame[y:y + h, x:x + w]
+            try:
+                grayFace = cv2.resize(grayFace, (emotionTargetSize))
+            except:
+                continue
+
+            grayFace = grayFace.astype('float32')
+            grayFace = grayFace / 255.0
+            grayFace = (grayFace - 0.5) * 2.0
+            grayFace = np.expand_dims(grayFace, 0)
+            grayFace = np.expand_dims(grayFace, -1)
+            emotion_prediction = emotionClassifier.predict(grayFace)
+            for i in range(7):
+                cv2.putText(frame, emotions[i]['emotion'] + " "+str(emotion_prediction[0][i]),
+                            (10, 10+i*20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            (0, 0, 0), 1, cv2.LINE_AA)
+
+            emotion_probability = np.max(emotion_prediction)
+            if (emotion_probability > 0.36):
+                emotion_label_arg = np.argmax(emotion_prediction)
+                color = emotions[emotion_label_arg]['color']
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                cv2.line(frame, (x, y + h), (x + 20, y + h + 20),
+                         color,
+                         thickness=2)
+                cv2.rectangle(frame, (x + 20, y + h + 20), (x + 110, y + h + 40),
+                              color, -1)
+                cv2.putText(frame, emotions[emotion_label_arg]['emotion'],
+                            (x + 25, y + h + 36), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                            (255, 255, 255), 1, cv2.LINE_AA)
+            else:
+                color = (255, 255, 255)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+            return emotion_probability
+
+        return 0
+
     def here_it_goes(self):
         report = []
         cap = cv2.VideoCapture(0)
@@ -91,8 +176,9 @@ class Engagement_Detection:
             if(ret==False):
                 break
 
+            
+            emotion = self.facial_emotion(frame)
             focused_level = self.detect_distraction(frame)
-            # emotion = self.facial_emotion(frame)
 
             # index = self.get_concentration_index(focused_level, emotion)
             # report.append(index)
